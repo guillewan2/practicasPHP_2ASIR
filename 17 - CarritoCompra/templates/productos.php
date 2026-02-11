16587d157eca264e8b1a513b1cd302a4d526caa2
Guille <guits0006@gmail.com>
2026-02-11 16:24:06 +0100

    Restore and adapt specific CSS for CarritoCompra

--- 17 - CarritoCompra/templates/productos.php
+++ 17 - CarritoCompra/templates/productos.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda</title>
-    <link rel="stylesheet" href="../../style.css">
+    <link rel="stylesheet" href="/17 - CarritoCompra/css/style.css">
 </head>
 
 <body>
